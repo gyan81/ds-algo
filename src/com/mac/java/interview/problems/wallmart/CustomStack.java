3cd6f9e56a636fad82c90c68157c6b2bf358a2d1
Monoj Karar <monoj_karar@hotmail.com>
2021-08-18 13:20:34 +0530

    New code added for the proeblems that asked in Wallmart 1st round of the interview(18-08-2021.

--- src/com/mac/java/interview/problems/wallmart/CustomStack.java
+++ src/com/mac/java/interview/problems/wallmart/CustomStack.java
@@ -1,2 +1,81 @@
-package com.mac.java.interview.problems.wallmart;public class CustomStack {
+package com.mac.java.interview.problems.wallmart;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Stack {
+    int[] stack = null;
+    int size;
+    int lastIndex = -1;
+    int minValue = Integer.MAX_VALUE;
+    Map<Integer, Integer> table = null;
+
+    public Stack(int size) {
+        this.size = size;
+        stack = new int[size];
+        table = new HashMap<>();
+    }
+
+    public void push(int i) {
+        if (lastIndex >= size - 1) {
+            System.out.println("Stack is Over flow");
+            return;
+        }
+        if (minValue > i) {
+            minValue = i;
+        }
+        stack[++lastIndex] = i;
+        table.put(lastIndex, minValue);
+        System.out.println("Push : " + i);
+    }
+
+    public void pop() {
+        if (lastIndex < 0) {
+            System.out.println("Stack is under flow.");
+            return;
+        }
+        int result = stack[lastIndex--];
+        findMin();
+        System.out.println("Pop : " + result);
+    }
+
+    private void findMin() {
+        if (lastIndex >= 0) {
+            minValue = table.get(lastIndex);
+        } else {
+            minValue = Integer.MAX_VALUE;
+        }
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public void peek() {
+        System.out.println("Peek : " + stack[lastIndex]);
+    }
+}
+
+public class CustomStack {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(10);
+        stack.push(5);
+        stack.push(8);
+        stack.peek();
+        System.out.println(stack.getMinValue());
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(stack.getMinValue());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.getMinValue());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.getMinValue());
+    }
 }
--- src/com/mac/java/interview/problems/wallmart/MeetingScheduler.java
+++ src/com/mac/java/interview/problems/wallmart/MeetingScheduler.java
@@ -1,2 +1,73 @@
-package com.mac.java.interview.problems.wallmart;public class MeetingScheduler {
+package com.mac.java.interview.problems.wallmart;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Meeting implements Comparable<Meeting> {
+    int startTime;
+    int endTime;
+
+    Meeting(int startTime, int endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public int compareTo(Meeting o) {
+        if (this.endTime < o.endTime) {
+            return -1;
+        } else if (this.endTime > o.endTime) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Meeting{" +
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
 }
+
+public class MeetingScheduler {
+
+    public int scheduler(int[] start, int[] end) {
+        int count = 0;
+        if (start.length > 0 && end.length > 0 && start.length == end.length) {
+            List<Meeting> meetings = new ArrayList<>();
+            int i = 0;
+            count = 1;
+            while (i < start.length) {
+                meetings.add(new Meeting(start[i], end[i]));
+                i++;
+            }
+            Collections.sort(meetings);
+            System.out.println(meetings);
+            i = 1;
+            Meeting m1 = meetings.get(0);
+            while (i < meetings.size()) {
+                Meeting m2 = meetings.get(i);
+                if (m1.endTime < m2.startTime) {
+                    m1 = m2;
+                    count++;
+                }
+                i++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+//        int[] start = {1, 3, 0, 5, 8, 8};
+//        int[] end = {2, 4, 6, 7, 9, 9};
+        int[] start = {1, 5,8,5,0,10,3};
+        int[] end = {2, 7,9,9,1,11,4};
+
+        MeetingScheduler meetingScheduler = new MeetingScheduler();
+        System.out.println(meetingScheduler.scheduler(start, end));
+    }
+}
+
